0b16e707d1951002ed45cbec6423b50595934e9a
VladGolosoff <v.golosoff@gmail.com>
2022-02-04 14:43:17 +0300

    Initial Commit

---
+++ Contacts/ContactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsApp.swift
+//  Contacts
+//
+//  Created by Владислав Голосов on 04.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
